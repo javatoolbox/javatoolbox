a76f40ebcd30307e0f5407df8348182f6e9d8128
Duncan <duncpro@icloud.com>
2025-07-12 20:06:16 -0400

    Make constructor-methods in `Options` public.

--- option/src/main/java/com/github/javatoolbox/option/Options.java
+++ option/src/main/java/com/github/javatoolbox/option/Options.java
@@ -2,11 +2,11 @@ package com.github.javatoolbox.option;
 
 public final class Options {
 
-    static <T> Option<T> some(T value) { return new Some<>(value); }
+    public static <T> Option<T> some(T value) { return new Some<>(value); }
 
-    static <T> Option<T> none() { return new None<>(); }
+    public static <T> Option<T> none() { return new None<>(); }
 
-    static <T> Option<T> maybe(T value) {
+    public static <T> Option<T> maybe(T value) {
         if (value == null)
             return none();
         else
